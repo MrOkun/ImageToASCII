7865c9707ea1c50508e1dd75af5b4fe9089e5f6f
Alexander Okun <alexanderklush3@gmail.com>
2022-11-12 19:59:46 +0300

    init

---
+++ ASCIIGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Text;
+using FastBitmapLib;
+using OkunGraphicLibrary.Algorithm;
+using OkunGraphicLibrary.Base;
+using ImageToASCII;
+
+namespace ASCIIGenerator
+{
+    internal class Program
+    {
+        private static string palette = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\" ^`'. ";
+
+        static void Main(string[] args)
+        {
+            Bitmap bitmap = (Bitmap)Bitmap.FromFile("img.png");
+
+            var imageToASCIIProcessor = new ImageToASCIIProcessor();
+
+            var ascii = imageToASCIIProcessor.Process(bitmap, palette);
+
+            using (StreamWriter sw = new("output.txt"))
+            {
+                sw.Write(ascii);
+            }
+
+            Console.WriteLine(ascii);
+        }
+    }
+}
---
+++ ImageToASCII/ImageToASCIIProcessor.cs
@@ -0,0 +1,46 @@
+﻿using FastBitmapLib;
+using OkunGraphicLibrary.Algorithm;
+using OkunGraphicLibrary.Base;
+using System.Drawing;
+using System.Text;
+
+namespace ImageToASCII
+{
+    public class ImageToASCIIProcessor
+    {
+        public string Process(Bitmap bitmap, string palette)
+        {
+            var colorConversion = new ColorConversion();
+            bitmap = colorConversion.Averge(bitmap);
+
+            FastBitmap fBitmap = new FastBitmap((Bitmap)bitmap.Fix());
+
+            Console.WriteLine($"Image size is {fBitmap.Width}, {fBitmap.Height}");
+
+            fBitmap.Lock();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < fBitmap.Height; i++)
+            {
+                sb.Append('\n');
+
+                for (int j = 0; j < fBitmap.Width; j++)
+                {
+                    Color color = fBitmap.GetPixel(j, i);
+
+                    int grayColorCoef = (int)Math.Round((double)(color.R + color.G + color.B) / 3, 0);
+
+                    int index = (int)Math.Round((decimal)palette.Length / 300 * grayColorCoef, 0);
+
+                    sb.Append(palette[index]);
+                    sb.Append(palette[index]);
+                }
+            }
+
+            fBitmap.Unlock();
+
+            return sb.ToString();
+        }
+    }
+}
